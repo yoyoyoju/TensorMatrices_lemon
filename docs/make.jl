efcce684db805ee789164096a487dec1f278c548
yoju LEE <yoyoyoju@gmail.com>
2017-06-16 16:59:28 +0200

    typo in docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ makedocs()
 # makedocs(modules=[TensorMatrices_lemon],
 # 		 doctest=true)
 # 
- deploydoc(deps	=Deps.pip("mkdocs", "python-markdown-math"),
+ deploydocs(deps	=Deps.pip("mkdocs", "python-markdown-math"),
  		  repo = "github.com/yoyoyoju/TensorMatrices_lemon.git",
  		  julia = "0.5",
  		  osname = "osx")
